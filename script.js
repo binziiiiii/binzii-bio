76b004e8e078e92a7c8ccebe0a38c307defc8fe9
binzii <strlticdjdjdj@gmail.com>
2026-09-21 20:40:49 -0400

    Update clipboard text and handle identifier

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const toast = document.querySelector('#toast');
 
 enterScreen.addEventListener('click', () => enterScreen.classList.add('is-hidden'));
 copyHandle.addEventListener('click', async () => {
-  await navigator.clipboard?.writeText('@107.4');
+  await navigator.clipboard?.writeText('@5rhk');
   toast.textContent = 'handle copied';
   toast.classList.add('show');
   setTimeout(() => toast.classList.remove('show'), 1500);
